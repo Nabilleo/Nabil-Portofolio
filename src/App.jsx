32663d44efe6f21b95fc897221614d59bb425d1d
Nabil <nabilleo21@gmail.com>
2026-03-08 10:31:33 +0700

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
       <div className="animate__animated animate__fadeInUp animate__delay-3s">
         <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
-          <img src="/assets/tools/favicon.png" className="w-10 rounded-md" loading="lazy" />
+          <img src="/assets/favicon.png" alt="Hero Image" className="w-10 rounded-md" loading="lazy" />
           <q>Kode yang indah, lahir dari ketekunan.😉</q>
         </div>
         <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Muhammad Nabil Hidayat</h1>
@@ -30,7 +30,6 @@ function App() {
     <div className="tentang mt-32 py-10" id="tentang">
       <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg" 
       data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">
-      <img src={DataImage.HeroImage} alt="Image" loading="lazy" />
         <p className="text-base/loose mb-10">
           Hi, perkenalkan saya Muhammad Nabil Hidayat, saya seorang pelajar SMKN 2 Nganjuk Jurusan 
           Pengembangan Perangkat Lunak Game, saya percaya bahwa desain dan fungsionalitas
